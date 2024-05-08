c74259542e30844911ead4a57bd0af4b9ee3376d
Siddharth Kesherwani <siddharthkesharwani2507@gmail.com>
2024-05-08 21:52:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer","Backend Developer","Full Stack"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop:true
+})
