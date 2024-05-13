b72e5b4431716f907f9c8ada6b7221c0dc69a0d0
Siddharth Kesherwani <siddharthkesharwani2507@gmail.com>
2024-05-13 12:26:45 +0530

    links added

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ var typed = new Typed(".text",{
     backDelay: 1000,
     loop:true
 })
+
+// Get a reference to the navbar element
+const navbar = document.querySelector('header .navbar');
+
+// Function to toggle the active class on the navbar
+function toggleNavbar() {
+    navbar.classList.toggle('active');
+}
+
+// Get a reference to the menu bar element (assuming it has an ID of "menu-bar")
+const menuBar = document.getElementById('menu-bar');
+
+// Add a click event listener to the menu bar
+menuBar.addEventListener('click', toggleNavbar);
